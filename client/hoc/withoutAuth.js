f822a97980f81b6b9f9ffe697bfba684ac7b6822
Samantha Monteiro <samantha_monteiro@intuit.com>
2020-07-07 00:56:50 -0700

    Removing comments

--- client/hoc/withoutAuth.js
+++ client/hoc/withoutAuth.js
@@ -1,9 +1,5 @@
 import withAuthRedirect from './withAuthRedirect';
 
-/**
- * Require the user to be unauthenticated in order to render the component.
- * If the user is authenticated, forward to the given URL.
- */
 export default function withoutAuth(WrappedComponent, location = '/admin/meetups') {
   return withAuthRedirect({
     WrappedComponent,
