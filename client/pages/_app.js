f6e8d62b9918d4a62dbb62049cfd884be36e7902
Samantha Monteiro <samantha_monteiro@intuit.com>
2020-07-13 20:27:49 -0700

    Remove unecessary stuff

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,21 +1,16 @@
-import { ApolloProvider } from "@apollo/react-hooks";
-
 import '../assets/scss/style.scss';
 import Layout from '../components/layout';
-import withData from "../lib/apollo";
 import { useFetchUser, UserProvider } from '../lib/user';
 
-const App = ({ Component, pageProps, apollo }) => {
+const App = ({ Component, pageProps }) => {
   const { user, loading } = useFetchUser();
   return (
-    <UserProvider value={{ user, loading}}>
-      <ApolloProvider client={apollo}>
-        <Layout user={user}>
-          <Component {...pageProps} />
-        </Layout>
-      </ApolloProvider>
+    <UserProvider value={{ user, loading }}>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
     </UserProvider>
   )
 };
 
-export default withData(App);
+export default App;
--- client/pages/admin/meetups.js
+++ client/pages/admin/meetups.js
@@ -1,38 +1,31 @@
-import Query from "../../components/query";
-import MEETUPS_QUERY from "../../apollo/queries/meetup/meetups";
-
 import { PrivateRoute } from '../../hocs/PrivateRoute';
+import api from '../../lib/api';
 
 
 const AdminMeetups = () => {
-    return (
-        <Query query={MEETUPS_QUERY} id={null}>
-        {({ data: { events } }) => {
-          return (
-            <div className='top-div'>
-                <div className='meetups'>
-                <div className='meetups-child'>
-                <h1>Upcoming Meetups</h1>
-                <div>
-                    {events.map((event) => {
-                        return (<div className='single-meetup' key={event.meetupId}>
-                        <div className='header'>
-                        <div>{event.date}</div>
-                        <div>{event.name}</div>
-                        <div >
-                        <a className='meetup-link' href={event.link} target='_blank'>Meetup</a>
-                        </div>
-                        </div>
-                        </div>)
-                    })}
+    const { response, error, isLoading} = api('/events');
 
-                </div>
-                </div>
-                </div>
+    return (
+        <div className='top-div'>
+            <div className='meetups'>
+            <div className='meetups-child'>
+            <h1>Upcoming Meetups</h1>
+            <div>
+                {!isLoading && response && response.map((event) => {
+                    return (<div className='single-meetup' key={event.meetupId}>
+                    <div className='header'>
+                    <div>{event.date}</div>
+                    <div>{event.name}</div>
+                    <div >
+                    <a className='meetup-link' href={event.link} target='_blank'>Meetup</a>
+                    </div>
+                    </div>
+                    </div>)
+                })}
             </div>
-        );
-        }}
-        </Query>
+            </div>
+            </div>
+        </div>
     );
 }
 
