7cfa6fed0768a15e3dc1edd619b609caa56f66d1
Samantha Monteiro <samantha_monteiro@intuit.com>
2020-07-13 21:38:08 -0700

    Add logic to create talk and not associate to user

--- client/pages/signup.js
+++ client/pages/signup.js
@@ -1,4 +1,36 @@
+import React, { useState } from 'react';
+
 const Signup = () => {
+
+    const [data, setData] = useState({
+        speakerName: '',
+        speakerEmail: '',
+        speakerPhone: '',
+        topic: '',
+        description: ''
+    });
+
+    const handleChange = e => {
+        setData({ ...data, [e.target.name]: e.target.value })
+    };
+
+    const register = async e => {
+        e.preventDefault();
+        const response = await fetch(`${process.env.API_BASE_URL}/talks`, {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+              speaker_name: data.speakerName,
+              speaker_email: data.speakerEmail,
+              speaker_phone: data.speakerPhone,
+              description: data.description,
+              topic: data.topic
+            })
+        });
+    }
+
     return (
         <div className='signUp-container'>
                 <div className='form-container'>
@@ -6,15 +38,15 @@ const Signup = () => {
                         <h3>Speaker Registration</h3>
                         <div>
                             <label htmlFor='speaker-name'>Name: </label>
-                            <input name='speakerName' id='speakerName' type='text'required />
+                            <input name='speakerName' id='speakerName' onChange={handleChange} type='text'required />
                         </div >
                         <div>
                             <label htmlFor='speaker-email'>Email: </label>
-                            <input type="email" id="speakerEmail" name="speakerEmail" required  />
+                            <input type="email" id="speakerEmail" onChange={handleChange} name="speakerEmail" required  />
                         </div>
                         <div>
                             <label htmlFor='speaker-phone'>Phone Number: </label>
-                            <input type="tel" name="phone" id='speakerPhone' required pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}" format="### ### ####" />
+                            <input type="tel" name="speakerPhone" id='speakerPhone' onChange={handleChange} required pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}" format="### ### ####" />
                         </div>
                         <div>
                             <label htmlFor='event-date'>Date Requested: </label>
@@ -24,14 +56,14 @@ const Signup = () => {
                         </div>
                         <div>
                             <label htmlFor='topic'>Topic:</label>
-                            <input name='topic' id='topic' type='text' required  />
+                            <input name='topic' id='topic' onChange={handleChange} type='text' required  />
                         </div>
                         <div>
                             <label htmlFor='description'>Description: </label>
-                            <textarea name='description' id='description' type='text' required />
+                            <textarea name='description' id='description' onChange={handleChange} type='text' required />
                         </div>
                         <div>
-                            <button className='btn' id='speaker-submit'>Submit</button>
+                            <button className='btn' id='speaker-submit' onClick={register}>Submit</button>
                         </div>
                     </form>
                 </div>
