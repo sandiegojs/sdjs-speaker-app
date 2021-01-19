2d516b1cf2e2a47487d5b026601dc533d748520c
Nathan Hannig <breakpoint@gmail.com>
2021-01-18 17:57:17 -0800

    Fix Meetups API call for day of month

--- server/config/functions/meetups.js
+++ server/config/functions/meetups.js
@@ -4,7 +4,7 @@ module.exports = async () => {
   return new Promise((resolve, reject) => {
 		let currentDate = new Date();
 		currentDate.setMonth(currentDate.getMonth() + 3);
-		const date = `${currentDate.getFullYear()}-${currentDate.getMonth()}-${currentDate.getDay()}`;
+		const date = `${currentDate.getFullYear()}-${currentDate.getMonth()}-${currentDate.getDate()}`;
 		axios.get(`https://api.meetup.com/sandiegojs/events?no_later_than=${date}`)
 		.then( response => resolve(response.data.map(event => ({
 			meetupId: event.id,
